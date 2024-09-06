c93b0794e776d594fc14360331251c7dab868ad2
Usman Hasan <usmanhasan197@gmail.com>
2024-09-06 09:36:39 +0500

    Update Q4.c

--- lab3/Q4.c
+++ lab3/Q4.c
@@ -6,12 +6,15 @@ ffuel = 118;
 bfuel = 123;
 printf("enter car fuel average : ");
 scanf("%d",&favg);
+if ( favg >0) 
+{
 fcost = (dist/favg)*ffuel + (dist/favg)*bfuel;
 fconsume = (dist*2)/favg;
 printf("\nfuel cost is : %d",fcost);
-printf("\nfuel consumed is : %d",fconsume);
+printf("\nfuel consumed is : %d",fconsume); 
+} 
+else printf("negative number try again");
 return 0;
 }
-
 
 
