81c53a8bbe1aafe760df887cb3c55fb6c9f921b4
Usman Hasan <usmanhasan197@gmail.com>
2024-09-26 19:42:29 +0500

    Create Q7.c

---
+++ Lab 5/Q7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+int x;
+printf("Enter a number: ");
+scanf("%d", &x);
+x % 10 == 0 ? 
+printf("Last Digit is Zero!") 
+  :
+printf ("Last Digit is Non-Zero!");
+
+return 0;
+}
