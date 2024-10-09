4ad1212272f7bbeaed8f482b13b36b73840d249c
Usman Hasan <usmanhasan197@gmail.com>
2024-10-09 21:04:08 +0500

    Create Q2.c

---
+++ Lab 6 /Q2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+  int i,Num,prime=0;
+ printf("enter a number");
+ scanf("%d",&Num);
+ for (i=2 ;i<Num ;i++)
+       { 
+         if(Num%i==0) 
+        prime=1;
+	     }
+	if (prime==0)
+  {
+printf("entered number %d is prime",Num);
+  }
+else printf("entered number is not prime ");
+return 0;
+}
